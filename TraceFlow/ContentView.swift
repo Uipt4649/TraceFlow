2646e28f1cb30598855e122940fe35a5caa73419
Ui <uipt1116@gmail.com>
2026-04-28 01:35:20 +0900

    init

---
+++ TraceFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TraceFlow
+//
+//  Created by 渡邉羽唯 on 2026/04/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TraceFlow/TraceFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceFlowApp.swift
+//  TraceFlow
+//
+//  Created by 渡邉羽唯 on 2026/04/28.
+//
+
+import SwiftUI
+
+@main
+struct TraceFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
